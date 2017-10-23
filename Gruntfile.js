206a655482a4258024a4d5735c38f030f68989d9
Lasse <lassetange@gmail.com>
2017-10-23 11:28:38 +0200

    Use assets_inline instead

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,11 +12,25 @@ module.exports = function(grunt) {
         }
       }
     },
+    assets_inline: {
+      all: {
+        options: {
+          inlineImg: true,
+          inlineSvg: true,
+          minify: true,
+        },
+        files: {
+          'index.html': 'assets/index.html'
+        }
+      },
+    },
     htmlmin: {
       dist: {
         options: { // Reference: https://github.com/kangax/html-minifier#options-quick-reference
           removeComments: true,
           collapseWhitespace: true,
+          minifyCSS: true,
+          minifyJS: true,
         },
         files: {
           'index.html': 'index.html'
@@ -26,10 +40,10 @@ module.exports = function(grunt) {
     watch: {
       scripts: {
         files: ['assets/**/*.html', 'assets/**/*.css',],
-        tasks: ['inlinecss'],
+        tasks: ['assets_inline'],
       }
     },
   });
 
-  grunt.registerTask('build', 'inlinecss', 'htmlmin');
+  grunt.registerTask('build', 'assets_inline', 'htmlmin');
 };
